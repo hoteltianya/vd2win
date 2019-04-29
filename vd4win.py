0c03f0ac5153aad255b909068f781d7959460c21
huangzhilin <hzlhn1990@outlook.com>
2019-04-29 14:54:17 +0800

    增加全局执行

--- vd4win.py
+++ vd4win.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 
 
+
 def ls_disk():
     ls = []
     disk_list = subprocess.Popen('smartctl --scan', stdout=subprocess.PIPE).stdout.readlines()
@@ -9,24 +10,29 @@ def ls_disk():
         ls.append(str(disk.split()[0], encoding="utf-8"))
     return ls
 
-def get_disk_smartinfo(disk = '/dev/sdc'):
+def get_disk_smartinfo(disk):
     smartlist = {}
     cmd = 'smartctl -a ' + disk
-    smartinfo = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout.readlines()
-    model = smartinfo[5].split()
-    if 'SSD' not in str(model[-1], encoding="utf-8"):
-        return None
-    sn = smartinfo[6].split()
-    if sn[0] == b'Serial':
-        smartlist['SN'] = sn[-1]
-    ac = smartinfo[66].split()
-    if ac[0] == b'172':
-        smartlist['172'] = int(ac[-1])
-    badblock = smartinfo[54].split()
-    if badblock[0] == b'5':
-        smartlist['05'] = int(badblock[-1])
-    # 返回smart信息的字典
-    print (smartlist)
+    smartinfo = subprocess.getoutput(cmd).split('\n')
+    # model = smartinfo[5].split()
+    for i in smartinfo:
+        if 'WDC' in i or 'TOSHIBA' in i:
+            return None
+        else:
+            if 'Serial' in i:
+                snlist = i.split()
+                smartlist['SN'] = snlist[-1]
+
+            if '172 Unknown_Attribute' in i:
+                smartlist['172'] = int(i.split()[-1])
+
+            if '5 Reallocated_Sector_Ct' in i:
+                smartlist['05'] = int(i.split()[-1])
+            if '171 Unknown_Attribute' in i:
+                smartlist['171'] = int(i.split()[-1])
+            if 'Firmware Version' in i:
+                smartlist['FW'] = i.split()[-1]
+    # print (smartlist)
     return smartlist
 
 
@@ -37,7 +43,16 @@ class Vdbench():
 
     def get_lun_list(self):
         self.lun_list = []
-        pass
+        cmd = 'wmic diskdrive'
+        diskinfo = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout.read()
+        # print(diskinfo)
+        # diskinfo = str(diskinfo,encoding="utf-8")
+        disk = diskinfo.split(b'\n')
+        for i in range(len(disk)):
+            #根据容量检查，删除指定容量的盘符
+            if '磁盘驱动器' in str(disk[i], encoding='ANSI') and disk[i].split()[-9] != b'1000202273280':
+                self.lun_list.append(str(disk[i].split()[-19],encoding='ANSI'))
+
 
     def creat_vd_config(self):
         sd=''
@@ -46,51 +61,49 @@ class Vdbench():
             sd = sd + 'sd=sd%d,lun=%s \n' % (i, lun)
             i = i+1
 
-        config = '''sd=default,threads=16,openflags=o_direct
+        config = '''sd=default,threads=16,openflags=directio
 %swd=wd1,sd=sd*,xfersize=1M,rdpct=0,seekpct=0
-rd=run1,wd=wd1,iorate=max,elapsed=600,interval=1
+rd=run1,wd=wd1,iorate=max,elapsed=6,interval=1
         ''' %sd
+
         with open('vdconfig', 'w') as f :
             f.write(config)
 
     def exec_vdbench(self):
         cmd = self.vd_path + ' -f vdconfig -o result'
-        subprocess.Popen(cmd)
-
+        subprocess.run(cmd)
+        # os.popen(cmd)
     def check_reult(self):
-        # with open('result/') as f :
-        #     if 'error' in f.read():
-        #         print('exec fail, please check disk')
-        #         sys.exit()
+        with open('result/errorlog.html') as f:
+            print(len(f.readlines()))
+            if  len(f.readlines())>4:
+                print('exec fail, please check disk')
+                sys.exit()
         disk_list = ls_disk()
+        print(disk_list)
         good_list = []
-        bad_list_05 = []
-        bad_list_172 = []
+        bad_list = []
         for disk in disk_list:
             smartinfo = get_disk_smartinfo(disk)
+            print(smartinfo)
             if smartinfo:
-                if '05' in smartinfo.keys() and smartinfo['05'] == 0:
-                    if '172' in smartinfo.keys() and smartinfo['172'] == 0:
-                        good_list.append(smartinfo['SN'])
-                    else:
-                        bad_list_172.append(smartinfo['SN'])
+                if smartinfo['05'] == 0 and smartinfo['172'] == 0 and smartinfo['171'] == 0:
+                    good_list.append(smartinfo['SN'])
                 else:
-                    bad_list_05.append(smartinfo['SN'])
+                    bad_list.append(smartinfo['SN'])
         for good in good_list:
             print('good list :\n \033[5;37;40m %s \033[0m'% good)
-        for bad_05 in bad_list_05:
-            print('bad list 05:\n \033[5;37;41m %s \033[0m'% bad_05)
-        for bad_172 in bad_list_172:
-            print('bad list 172:\n \033[5;34;43m %s \033[0m' % bad_172)
-
+        for bad in bad_list:
+            print('bad list :\n \033[5;31;40m %s \033[0m'% bad)
     def run(self):
         self.get_lun_list()
         self.creat_vd_config()
         self.exec_vdbench()
         self.check_reult()
 
-a = Vdbench()
+a = Vdbench(r'C:\Users\sumu\Desktop\vd\vdbench50403\vdbench.bat')
 a.run()
+
 
 
 
---
+++ vdbench50403/linux/config.sh
@@ -0,0 +1,28 @@
+#!/bin/ksh
+
+#
+# Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+#
+
+#
+# Author: Henk Vandenbergh.
+#
+
+
+
+echo `date`;echo ">>>>>uname -a"
+uname -a
+
+echo `date`;echo ">>>>>ifconfig -a"
+ifconfig -a
+
+echo `date`;echo ">>>>>id"
+id
+
+echo `date`;echo ">>>>>mount"
+mount
+
+# Show if there are any other vdbench tests running:
+echo `date`;echo ">>>>>ps -ef | grep -i vdb"
+ps -ef | grep -i vdb
+
