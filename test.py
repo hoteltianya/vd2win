9e3a18ce29dcdb8a3364af852c12c7d2f0f9a911
h zl <hzlhn@qq.com>
2019-05-13 03:19:13 +0800

    增加解析smart功能，目前可以脱离smartctl使用

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def get_wmi_smartinfo():
     wmi = GetObject('winmgmts:/root/wmi')
     processes = wmi.ExecQuery('SELECT * FROM MSStorageDriver_ATAPISmartData')
     time.sleep(5)
-    # print(processes)
+    print(processes)
     for process in processes:
         diskdict[process.InstanceName] = process.VendorSpecific
     # print(diskdict)
@@ -45,10 +45,38 @@ class smartformwin():
             self.diskdict[disk[2]] = disk
         print(self.diskdict)
         # self.wmi_name_vs = get_wmi_smartinfo()
+    def parise_smart(self):
+        self.smartinfolist=[]
+        for key in self.diskdict:
+            if len(self.diskdict[key]) > 5:
+                info = {}
+                info['ID'] = key
+                info['Model'] = self.diskdict[key][0]
+                info['SN'] = self.diskdict[key][3]
+                info['FW'] = self.diskdict[key][1]
+                A_smart = list(self.diskdict[key][-1])
+                del A_smart[0]
+                del A_smart[0]
+                l = A_smart
+                n=12
+                A_smarlist = [l[i:i + n] for i in range(0, len(l), n)]
+                for A in A_smarlist:
+                    print(A)
+                    if A[0] == 5:
+                        info['05'] = A[5] + 256*A[6]
+                    if A[0] ==160:
+                        info['160'] = A[5] + 256 * A[6]
+                    if A[0] ==171:
+                        info['171'] = A[5] + 256 * A[6]
+                    if A[0] ==172:
+                        info['172'] = A[5] + 256 * A[6]
+                self.smartinfolist.append(info)
+        print(self.smartinfolist)
 
 
 
 
 a=smartformwin()
 a.get_wmi_disk_basic()
+a.parise_smart()
 # get_wmi_smartinfo()
