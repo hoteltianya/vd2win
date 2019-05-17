0ee183ea180ea77fef164ace27d856a26aed5146
huangzhilin <hzlhn1990@outlook.com>
2019-05-17 15:39:19 +0800

    修改获取smart信息的函数

--- vd4win.py
+++ vd4win.py
@@ -15,11 +15,10 @@ def get_disk_smartinfo(disk):
     # 还不知道其他的工具，因此需要安装smartmontools程序
     smartlist = {}
     cmd = 'smartctl -a ' + disk
-    smartinfo = subprocess.getoutput(cmd).split('\n')
-    for i in smartinfo:
-        if 'WDC' in i or 'TOSHIBA' in i or 'Virtual' in i:
-            return None
-        else:
+    smartinfo = subprocess.getoutput(cmd)
+
+    if 'SSDC' in disk or 'SSDE' in disk or 'GG0' in disk or 'SC36' in disk or 'JEYI' in disk or 'AMST' in disk:
+        for i in smartinfo.split('\n'):
             if 'Serial' in i:
                 snlist = i.split()
                 smartlist['SN'] = snlist[-1]
@@ -61,14 +60,14 @@ class Vdbench():
 hd=localhost,jvms=8
 sd=default,threads=16,openflags=directio
 %swd=wd1,sd=sd*,xfersize=1M,rdpct=0,seekpct=0
-rd=run1,wd=wd1,iorate=max,elapsed=600,interval=1
+rd=run1,wd=wd1,iorate=max,elapsed=6,interval=1
         ''' %sd
         with open('vdconfig', 'w') as f :
             f.write(config)
 
     def exec_vdbench(self):
         cmd = self.vd_path + ' -f vdconfig -o result'
-        self.vdlog = subprocess.getoutput(cmd)
+        subprocess.run(cmd)
 
     def check_reult(self):
         with open('result/errorlog.html') as f:
@@ -80,14 +79,22 @@ rd=run1,wd=wd1,iorate=max,elapsed=600,interval=1
         print(disk_list)
         self.good_list = []
         self.bad_list = []
+        self.smartinfo = []
         for disk in disk_list:
             smartinfo = get_disk_smartinfo(disk)
+            self.smartinfo.append(smartinfo)
             print(smartinfo)
             if smartinfo:
                 if smartinfo['05'] == 0 and smartinfo['172'] == 0 and smartinfo['171'] == 0 and smartinfo['160'] == 0:
                     self.good_list.append(smartinfo['SN'])
                 else:
                     self.bad_list.append(smartinfo['SN'])
+        self.goodstr = ''
+        self.badstr = ''
+        for sn in self.good_list:
+            self.goodstr = self.goodstr + '\n \n'+sn
+        for sn in self.bad_list:
+            self.badstr = self.badstr + '\n \n'+sn
         for good in self.good_list:
             print('good list :\n \033[5;37;40m %s \033[0m'% good)
         for bad in self.bad_list:
@@ -99,8 +106,42 @@ rd=run1,wd=wd1,iorate=max,elapsed=600,interval=1
         self.exec_vdbench()
         self.check_reult()
 
-a = Vdbench()
-a.run()
+import tkinter as tk
+import tkinter.messagebox
+win = tk.Tk()
+win.title('vdbench test')
+win.geometry('800x600')
+
+var1 = tk.StringVar()
+var2 = tk.StringVar()
+var3 = tk.StringVar()
+
+var3.set('开始测试')
+# run_log = tk.Text(win, height=25)
+# run_log.pack()
+run_flag = False
+def run_vdbench():
+    global run_log
+    global run_flag
+    run_flag =True
+    if run_flag:
+        # tkinter.messagebox.showinfo('警告','确定开始测试吗？')
+        a = Vdbench()
+        a.run()
+        print(a.good_list)
+        print(a.bad_list)
+        var1.set(a.goodstr)
+        var2.set(a.badstr)
+        # run_log.insert('end', a.vdlog)
+        run_flag = False
+
+
+good_l = tk.Label(win, textvariable=var1, bg='grey', fg='green', width=60, height=10, font=('Arial',12)).pack()
+space = tk.Label(win, bg='white', fg='green', width=60, height=1, ).pack()
+bad_l = tk.Label(win, textvariable=var2, bg='grey', fg='red', width=60, height=10,font=('Arial', 12)).pack()
+b = tk.Button(win, textvariable=var3, width=15, height=2, command=run_vdbench).pack()
+
+win.mainloop()
 
 
 
